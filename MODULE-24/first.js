9fa7c73cc66f6080e5be7548bcb3538cbf082c4a
Affnan Sawad <affnansawad2002@gmail.com>
2024-06-21 16:29:12 +0600

    DOM STARTED

--- MODULE-24/first.js
+++ MODULE-24/first.js
@@ -2,3 +2,10 @@
 
 // MODULE - 24 => DOM INTRODUCTION.
 
+// What is the DOM?
+
+// The Document Object Model (DOM) is a programming interface for web documents. It represents the structure of a document as a tree of objects. This model allows programs and scripts to dynamically access and update the content, structure, and style of documents.
+
+// HOW To add JS FILE IN HTML FILE  ??
+
+//  < / BODY > ER AGE <SCRIPT SRC = " JS FILE NAME " > </SCRIPT>
